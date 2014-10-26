17f7b9617d79671b9bd3008e17b032b17836f336
Wai Kit <linwaikit@gmail.com>
2014-10-26 12:14:06 +0800

    Added woocommerce

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -13,11 +13,12 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php get_sidebar( 'main' ); ?>
-
+			<!-- waikit: disable credits
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+			</div>
+			-->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
